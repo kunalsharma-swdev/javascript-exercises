46d3ec87a2fd380872df27566883177bae65d9cd
Kunal Sharma <kunalsharma.swdev@gmail.com>
2026-06-21 00:57:19 +0530

    hih

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let len = str.length;
+    let reversed = "";
+    for (let i = len - 1; i > -1; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
