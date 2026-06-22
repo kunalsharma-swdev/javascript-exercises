0cb677e4c18ed248bba2109a75043f0136dbb36e
Kunal Sharma <kunalsharma.swdev@gmail.com>
2026-06-22 17:45:25 +0530

    done the 8th exercise of js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...arr1) {
+    
+    let arr1len = arr1.length;
+    function filt(num){
+        for(let i=0;i<arr1len;i++){
+            if(num===arr1[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    let finalarr = arr.filter(filt);
+    return finalarr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
