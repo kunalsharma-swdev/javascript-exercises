22da256e755d6f1b988991d11eb324a3655fd21f
kunalsharma-swdev <kunalsharma.swdev@gmail.com>
2026-07-20 22:46:45 +0530

    done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Convert to lowercase and remove non-alphanumeric characters
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
 };
+
+module.exports = palindromes;
 
 // Do not edit below this line
 module.exports = palindromes;
