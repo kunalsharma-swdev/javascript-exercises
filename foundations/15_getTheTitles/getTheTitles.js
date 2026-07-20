1f8e742964936f7f64ec3b8d266bb7630b9dccba
kunalsharma-swdev <kunalsharma.swdev@gmail.com>
2026-07-20 22:56:03 +0530

    hello

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+let x =[];
+const getTheTitles = function(x) {
+    let y = [];
+    for(let i=0;i<x.length;i++){
+        y.push(x[i].title);
+    }
+    return y;
 };
 
 // Do not edit below this line
