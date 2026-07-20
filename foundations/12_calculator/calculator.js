40b8ee501529e503674a88df88817628e147c615
kunalsharma-swdev <kunalsharma.swdev@gmail.com>
2026-07-20 22:22:02 +0530

    ddd

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+let x;
+let y;
+let z =[];
+const add = function(x,y) {
+  return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(z) {
+  let summ =0;
+  for(let i=0;i<z.length;i++){
+    summ+=z[i];
+  }
+  return summ;
 };
 
-const multiply = function() {
-
+const multiply = function(z) {
+  let summ =1;
+  for(let i=0;i<z.length;i++){
+    summ = summ*z[i];
+  }
+  return summ;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	summ=1;
+  for(let i=0;i<y;i++){
+    summ = summ*x;
+  }
+  return summ;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  summ=1;
+	for(let i=x;i>1;i--){
+    summ=summ*i;
+  }
+  return summ;
 };
 
 // Do not edit below this line
