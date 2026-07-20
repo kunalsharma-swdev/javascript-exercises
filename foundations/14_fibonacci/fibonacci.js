a1a746d9a8a719f69023dfb0370afbd2df3b108d
kunalsharma-swdev <kunalsharma.swdev@gmail.com>
2026-07-20 22:51:26 +0530

    done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = Number(n);
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n === 1) {
+        return 1;
+    }
+
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return second;
 };
 
+module.exports = fibonacci;
 // Do not edit below this line
 module.exports = fibonacci;
