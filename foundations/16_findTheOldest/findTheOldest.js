0980b171e00d913ee760c3f99cd8f15460a8e1fe
kunalsharma-swdev <kunalsharma.swdev@gmail.com>
2026-07-20 23:08:35 +0530

    add

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (x) {
+    let oldest;
+    let current = 0;
 
+    for (let i = 0; i < x.length; i++) {
+        let age = x[i].yearOfDeath - x[i].yearOfBirth;
+
+        if (age > current) {
+            current = age;
+            oldest = x[i];
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
